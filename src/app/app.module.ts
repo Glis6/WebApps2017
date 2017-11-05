5b2a532fe68a9cc1afa54eb954242510d816b003
Gilles Vercammen <gilles.vercammen.v4433@student.hogent.be>
2017-11-05 15:04:43 +0100

    Postinstall attempt #13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {AngularFireModule} from "angularfire2";
 import {AngularFireAuthModule} from "angularfire2/auth";
 import {AngularFirestoreModule} from "angularfire2/firestore";
-import {environment} from "../environments/environment.prod";
+import {environment} from "../environments/environment";
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
