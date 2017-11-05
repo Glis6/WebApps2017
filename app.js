d59bcbdb0cbec6ac2e378d7b7527d83fe5e0b71c
Gilles Vercammen <gilles.vercammen.v4433@student.hogent.be>
2017-11-05 14:43:44 +0100

    Postinstall attempt #7

--- app.js
+++ app.js
@@ -4,14 +4,28 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.use(forceSSL());
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 const port = process.env.PORT || '3001';
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+
+server.listen(port, function () {
+  console.log('Server is live!');
+});
