edba8b62805b591dfa221ccc788f94ab14ebc4c7
Gilles Vercammen <gilles.vercammen.v4433@student.hogent.be>
2017-12-06 16:13:02 +0100

    Linking to all pages

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ require('./models/Drawing');
 require('./models/User');
 require('./config/passport');
 
-mongoose.connect(process.env.DATABASE_URL, { useMongoClient: true });
+mongoose.connect(process.env.DATABASE_URL, {useMongoClient: true});
 
 var index = require('./routes/index');
 var user = require('./routes/user');
 
 var app = express();
-
 
 
 // view engine setup
@@ -29,23 +28,28 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(passport.initialize());
 
 app.use('/', index);
 app.use('/API/user', user);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.all('*', function (req, res) {
+  const indexFile = path.join(__dirname, 'dist') + '/index.html';
+  res.status(200).sendFile(indexFile);
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
